fc1335c6129a76e882eb40d27e78f18e5518a45d
petrusenko <petrusenko@tutu.tech>
2019-11-15 13:16:44 +0300

    port

--- python/app/main.py
+++ python/app/main.py
@@ -6,9 +6,9 @@ def hello():
     return "Hello World from Flask"
 
 @app.route("/main.py")
-def hello():
+def hello_main():
     return "Hello World MAIN.PY"
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=3333)
+    app.run(host='0.0.0.0', debug=True, port=80)
